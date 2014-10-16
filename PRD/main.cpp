3d7cb34a0824b991c04e78b9f28ef2e515663d2d
zhangfan <zhangfan@joyschool.cn>
2014-10-16 23:05:41 +0800

    change the increment to exponential increase

--- PRD/main.cpp
+++ PRD/main.cpp
@@ -2,12 +2,13 @@
 #include <vector>
 #include <cstdlib>
 #include <ctime>
+#include <cmath>
 
 using namespace std;
 
 class RandomList {
 private :
-    vector<int> *weights;            // possibilities.
+    vector<ulong> *weights;            // possibilities.
     int getSum();
 public:
     RandomList(int size);
@@ -18,7 +19,7 @@ public:
 };
 
 RandomList::RandomList(int size){
-    this->weights = new vector<int>(size);
+    this->weights = new vector<ulong>(size);
     fill(this->weights->begin(), this->weights->end(), 1);
     srand(time(0));
 }
@@ -46,27 +47,49 @@ int RandomList::getRandomElement() {
 int RandomList::getPseudoRandomElement() {
     int randomindex = rand() % this->getSum();
     int target = 0;
-    // found i when it stops
+    // found target when it stops
     while(target < this->getSize() && (randomindex -= this->weights->at(target)) > 0) {
         target++;
     }
     // auto calibrate the possibilities.
     for(int i=0; i<this->weights->size(); i++) {
-        i != target ? this->weights->at(i) += 1000 : this->weights->at(i) = 1;
+      if(i != target){
+          this->weights->at(i) == 1 ? this->weights->at(i) = 100 : this->weights->at(i) *= 2;
+      }
+      else {
+          this->weights->at(i) = 1;
+      }
     }
     return target;
+}
+
+// calculate the Var(X)
+double calvar(vector<uint> *v) {
+    ulong sum = 0;
+    for (uint &x : *v) {
+        sum += x;
+    }
+    ulong mean = sum / v->size();
+
+    ulong variance = 0;
+    for (uint &x : *v) {
+        variance += (x - mean) * (x - mean);
+    }
+    return sqrt(variance);
+
 }
 
 // Test function
 int main() {
     int range, tries;
+    cout.precision(3);
     while(1) {
         cout << "please input the range of selection: ";
         cin >> range;
         cout << "please input how many tries you like to have : ";
         cin >> tries;
-        vector<int> *ro = new vector<int>(range);
-        vector<int> *pro = new vector<int>(range);
+        vector<uint> *ro = new vector<uint>(range);
+        vector<uint> *pro = new vector<uint>(range);
         RandomList* rl = new RandomList(range);
         RandomList* prl = new RandomList(range);
 
@@ -85,9 +108,12 @@ int main() {
         for(int i=0; i<pro->size(); i++) {
             cout << i << ": " << pro->at(i) << "\n";
         }
+        cout << "The Variance is " << calvar(pro) << endl;
+
         cout << "Random Distribution: \n";
         for(int i=0; i<ro->size(); i++) {
             cout << i << ": " << ro->at(i) << "\n";
         }
+        cout << "The Variance is " << calvar(ro) << endl;
     }
 }
